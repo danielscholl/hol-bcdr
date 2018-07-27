98832414e4e7fb773a71e108ff6b0d0a40a7e713
danielscholl <daniel@scholllopez.com>
2018-07-27 12:41:53 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,6 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 #Assign Packages to Install
 $Packages = 'googlechrome', `
   'visualstudiocode', `
-  'git', `
   'visualstudio2017community', `
   'visualstudio2017-workload-azure', `
   'visualstudio2017-workload-netweb'
