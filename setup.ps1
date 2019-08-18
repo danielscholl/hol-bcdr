0b800d28c600d4269d87c10364f755fa55b15c50
danielscholl <dascholl@microsoft.com>
2019-08-18 09:50:37 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -3,7 +3,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Assign Packages to Install
-$Packages = 'googlechrome', 'visualstudiocode', 'docker-desktop'
+$Packages = 'googlechrome', 'visualstudiocode', 'docker-desktop', 'azure-cli'
 #  'visualstudio2017community', `
 #  'visualstudio2017-workload-azure', `
 #  'visualstudio2017-workload-netweb'
